45d86113626966a42589939fd4cec26d83608ad4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-08-23 22:35:52 +0200

    make steps run slower to better see the effects in the ui

--- feature/src/main/java/glue/Steps.java
+++ feature/src/main/java/glue/Steps.java
@@ -5,43 +5,62 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Steps {
+
     @Given("^alpha$")
     public void alpha() throws Throwable {
+        sleep();
     }
 
     @Given("^A$")
     public void a() throws Throwable {
+        sleep();
     }
 
     @When("^B$")
     public void b() throws Throwable {
+        sleep();
     }
 
     @Then("^C$")
     public void c() throws Throwable {
+        sleep();
     }
 
     @Given("^person$")
     public void person() throws Throwable {
+        sleep();
     }
 
     @When("^jump$")
     public void jump() throws Throwable {
+        sleep();
     }
 
     @Then("^up$")
     public void up() throws Throwable {
+        sleep();
     }
 
     @Given("^rabbit$")
     public void rabbit() throws Throwable {
+        sleep();
     }
 
     @When("^duck$")
     public void duck() throws Throwable {
+        sleep();
     }
 
     @Then("^hidden$")
     public void hidden() throws Throwable {
+        sleep();
+    }
+
+    private void sleep(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
